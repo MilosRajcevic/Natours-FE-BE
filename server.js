ebe94aff86ee6965bf2c4e06c77bf9ab011052e4
Milos Rajcevic <rajcevicmilos3@gmail.com>
2022-11-30 23:06:29 +0100

    - Implementing mongoose and connect our MongoDb database

--- server.js
+++ server.js
@@ -1,7 +1,21 @@
+const mongoose = require('mongoose');
 const dotnev = require('dotenv').config({ path: './config.env' });
 const app = require('./app');
 
 const port = process.env.PORT || 3000;
+const db = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+
+main().catch((err) => console.log(err));
+
+async function main() {
+  mongoose
+    .connect(db)
+    .then((connection) => console.log('DB connection successuful'));
+}
+
 app.listen(port, () => {
   console.log(`App runing on port ${port}...`);
 });
