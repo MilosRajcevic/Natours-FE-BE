9874f7b448297d720245bf1a99d9f3bdbfbfb7e3
Milos Rajcevic <rajcevicmilos3@gmail.com>
2022-12-01 14:00:05 +0100

    - Update tourModel with all necessary fields, types and options

--- models/tourModel.js
+++ models/tourModel.js
@@ -5,12 +5,49 @@ const toursSchema = new mongoose.Schema({
     type: String,
     required: [true, 'A tour must have a name'],
     unique: true,
+    trim: true,
   },
-  rating: { type: Number, default: 4.5 },
+  duration: {
+    type: Number,
+    required: [true, 'A tour must have a duration'],
+  },
+  maxGroupSize: {
+    type: Number,
+    required: [true, 'A tour must have group size'],
+  },
+  difficulty: {
+    type: String,
+    required: [true, 'A tour should have a difficulity'],
+  },
+  ratingsAverage: { type: Number, default: 4.5 },
+  ratingsQuantity: { type: Number, default: 0 },
   price: {
     type: Number,
     required: [true, 'A tour must have a price'],
   },
+  priceDiscount: {
+    type: Number,
+  },
+  summary: {
+    type: String,
+    // Trim dont allow us to have a bunch of white space like: '    Bla bla    bla '
+    trim: true,
+    required: [true, 'A tour must have description'],
+  },
+  description: {
+    type: String,
+    trim: true,
+  },
+  imageCover: {
+    type: String,
+    required: [true, 'A tour must have a cover image'],
+  },
+  images: [String],
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
+  startDates: [Date],
 });
 
 const Tour = mongoose.model('Tour', toursSchema);
