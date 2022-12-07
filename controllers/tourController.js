ee7ee1a6231a77c5e298be9ddf41730549b938f8
Milos Rajcevic <rajcevicmilos3@gmail.com>
2022-12-07 14:47:41 +0100

    - Implementing basic filtering feature for getting all tours, and prepare our query for more features like sort, page, limit, etc

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,9 +1,28 @@
+const { query } = require('express');
 const Tour = require('../models/tourModel');
 
 exports.getAllTours = async (req, res) => {
   try {
-    const tours = await Tour.find();
+    // BUILD QUERY
+    const queryObj = { ...req.query };
+    const excludedFields = ['page', 'sort', 'limit', 'fields'];
+    excludedFields.forEach((el) => delete queryObj[el]);
 
+    const query = Tour.find(queryObj);
+
+    console.log(req.query, queryObj);
+
+    // EXECUTE QUERY
+    const tours = await query;
+
+    // Same thing like above, just different methods
+    // const tours = await Tour.find()
+    //   .where('duration')
+    //   .equals(5)
+    //   .where('difficulity')
+    //   .equals('easy');
+
+    // SEND RESPONSE
     res.status(200).json({
       status: 'success',
       results: tours.length,
@@ -14,7 +33,7 @@ exports.getAllTours = async (req, res) => {
   } catch (error) {
     res.status(404).json({
       status: 'failed',
-      message: error,
+      message: error.message,
     });
   }
 };
