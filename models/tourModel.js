0793c2ba232e4b26fc6169079ce95dd71a0ed191
Milos Rajcevic <rajcevicmilos3@gmail.com>
2022-12-14 16:44:28 +0100

    - Implementing document, query, aggregation Mongoose middleware in tourModel

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const slugify = require('slugify');
 
 const toursSchema = new mongoose.Schema(
   {
@@ -8,6 +9,7 @@ const toursSchema = new mongoose.Schema(
       unique: true,
       trim: true,
     },
+    slug: String,
     duration: {
       type: Number,
       required: [true, 'A tour must have a duration'],
@@ -50,6 +52,10 @@ const toursSchema = new mongoose.Schema(
       select: false,
     },
     startDates: [Date],
+    secretTour: {
+      type: Boolean,
+      default: false,
+    },
   },
   {
     toJSON: { virtuals: true },
@@ -58,6 +64,44 @@ const toursSchema = new mongoose.Schema(
 
 toursSchema.virtual('durationWeeks').get(function () {
   return this.duration / 7;
+});
+
+// DOCUMENT MIDDLEWARE: runs before .save() and .create()
+toursSchema.pre('save', function (next) {
+  this.slug = slugify(this.name, { lower: true });
+  next();
+});
+
+// eslint-disable-next-line prefer-arrow-callback
+// toursSchema.pre('save', function (next) {
+//   console.log('Will save document...');
+//   next();
+// });
+
+// toursSchema.post('save', function (doc, next){
+//   console.log(doc);
+//   next();
+// });
+
+// QUERY MIDDLEWARE
+
+// toursSchema.pre('find', function (next) {
+toursSchema.pre(/^find/, function (next) {
+  this.find({ secretTour: { $ne: true } });
+
+  this.start = Date.now();
+  next();
+});
+
+toursSchema.post(/^find/, function (docs, next) {
+  console.log(`Query took ${Date.now() - this.start}, milliseconds`);
+  next();
+});
+
+// AGGREGATION MIDDLEWARE
+toursSchema.pre('aggregate', function (next) {
+  this.pipeline().unshift({ $match: { secretTour: { $ne: true } } });
+  next();
 });
 
 const Tour = mongoose.model('Tour', toursSchema);
