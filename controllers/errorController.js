a449e6f08f423d938e9e09ab33442cc35b7818c9
Milos Rajcevic <rajcevicmilos3@gmail.com>
2022-12-15 22:33:04 +0100

    - Separate and handling errors in production and development environment

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,11 +1,40 @@
-// Middleware for globarErrorHandler
+const sendErrorDev = (dev, res) => {
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message,
+    error: err,
+    stack: err.stack,
+  });
+};
 
+const sendErrorProd = (dev, res) => {
+  // Operational, trusted error: send message to client
+  if (err.isOperational) {
+    res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message,
+    });
+    // Programing or other unknown error: don't leak error details
+  } else {
+    // 1) Log error
+    console.log('ERROR 💥💥💥', err);
+
+    // 2) Send generic message
+    res.status(500).json({
+      status: 'error',
+      message: 'Someting went very wrong',
+    });
+  }
+};
+
+// Middleware for globarErrorHandler
 module.exports = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
   err.status = err.status || 'error';
 
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message,
-  });
+  if (process.env.NODE_ENV === 'developmnet') {
+    sendErrorDev(err, res);
+  } else if (procces.env.NODE_ENV === 'production') {
+    sendErrorProd(dev, res);
+  }
 };
