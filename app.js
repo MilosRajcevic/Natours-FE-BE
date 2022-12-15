e97d29e1a195b00f6677a45a58759fa227ffb0e4
Milos Rajcevic <rajcevicmilos3@gmail.com>
2022-12-15 14:52:58 +0100

    - Create global error handler function, and refactoring code for handling error on undefined routes

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const morgan = require('morgan');
 
+const AppError = require('./utils/appError');
+const globalErrorHandler = require('./controllers/errorController');
 const tourRouter = require('./routes/tourRoutes');
 const usersRouter = require('./routes/userRoutes');
 
@@ -27,11 +29,14 @@ app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', usersRouter);
 
 app.all('*', (req, res, next) => {
-  res.status(404).json({
-    status: 'Fail',
-    message: `Can't find ${req.originalUrl}`,
-  });
-  next();
+  // const err = new Error(`Can't find ${req.originalUrl} on this server!`);
+  // err.status = 'fail';
+  // err.statusCode = 404;
+  // next(err);
+  // We replace code above with new AppError class
+  next(new AppError(`Can't find ${req.originalUrl} on this server!`), 404);
 });
+
+app.use(globalErrorHandler);
 
 module.exports = app;
