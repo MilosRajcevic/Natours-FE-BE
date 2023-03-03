8dfee6e2362a0225948d3ad7c91fbee34574b4c1
Milos Rajcevic <rajcevicmilos3@gmail.com>
2023-03-03 18:52:16 +0100

    - Implementing Mapbox with few changes in helmet and base block head

---
+++ public/js/mapbox.js
@@ -0,0 +1,50 @@
+/* eslint-disable */
+
+const locations = JSON.parse(document.getElementById('map').dataset.locations);
+
+mapboxgl.accessToken =
+  'pk.eyJ1IjoibWlsb3MwMjMiLCJhIjoiY2xlcTd2cmJwMGptcTN5cDRwdHF4ejIwcSJ9.Yv1OA1IlETi4RDU8GmtgwQ';
+const map = new mapboxgl.Map({
+  container: 'map', // container ID
+  style: 'mapbox://styles/milos023/cleq8szr2002f01s5617r4pn8', // style URL
+  // center: [-118.313571, 33.987787], // starting position [lng, lat]
+  // zoom: 10, // starting zoom
+  // interactive: false,
+  scrollZoom: false,
+});
+
+const bounds = new mapboxgl.LngLatBounds();
+
+locations.forEach((loc) => {
+  // Create marker
+  const el = document.createElement('div');
+  el.className = 'marker';
+
+  // Add marker
+  new mapboxgl.Marker({
+    element: el,
+    anchor: 'bottom',
+  })
+    .setLngLat(loc.coordinates)
+    .addTo(map);
+
+  // Add popup
+  new mapboxgl.Popup({
+    offset: 30,
+  })
+    .setLngLat(loc.coordinates)
+    .setHTML(`<p>Day ${loc.day}: ${loc.description}</p>`)
+    .addTo(map);
+
+  // Extends map bounds to include current location
+  bounds.extend(loc.coordinates);
+});
+
+map.fitBounds(bounds, {
+  padding: {
+    top: 200,
+    bottom: 150,
+    left: 100,
+    right: 100,
+  },
+});
