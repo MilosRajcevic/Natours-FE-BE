0b9fc7c4988dc0d6bf578a9a6bd6cfbac048bc89
Milos Rajcevic <rajcevicmilos3@gmail.com>
2023-03-23 22:43:49 +0100

    - Implementing logout logic

--- controllers/authController.js
+++ controllers/authController.js
@@ -110,32 +110,48 @@ exports.protect = catchAsyncError(async (req, res, next) => {
   next();
 });
 
+// We want to set new cookie which one will no make sanse and delete him ASAP, because
+// httpOnly: true doenst allow us to do modifications with cookie, delete, update, etc...
+// Key is to give same name like original cookie
+exports.logout = (req, res) => {
+  res.cookie('jwt', 'loggedout', {
+    expires: new Date(Date.now() + 10 * 1000),
+    httpOnly: true,
+  });
+
+  res.status(200).json({ status: 'success' });
+};
+
 // Only for render pages, no error
-exports.isLoggedIn = catchAsyncError(async (req, res, next) => {
+exports.isLoggedIn = async (req, res, next) => {
   if (req.cookies.jwt) {
-    // 1) Verification token
-    const decoded = await promisify(jwt.verify)(
-      req.cookies.jwt,
-      process.env.JWT_SECRET
-    );
+    try {
+      // 1) Verification token
+      const decoded = await promisify(jwt.verify)(
+        req.cookies.jwt,
+        process.env.JWT_SECRET
+      );
 
-    // 2) Check if user still exists
-    const currentUser = await User.findById(decoded.id);
-    if (!currentUser) {
+      // 2) Check if user still exists
+      const currentUser = await User.findById(decoded.id);
+      if (!currentUser) {
+        return next();
+      }
+
+      //3) Check if user changed password after the token was issued
+      if (currentUser.changedPasswordAfter(decoded.iat)) {
+        return next();
+      }
+
+      // THERE IS A LOGGED IN USER
+      res.locals.user = currentUser;
+      return next();
+    } catch (err) {
       return next();
     }
-
-    //3) Check if user changed password after the token was issued
-    if (currentUser.changedPasswordAfter(decoded.iat)) {
-      return next();
-    }
-
-    // THERE IS A LOGGED IN USER
-    res.locals.user = currentUser;
-    return next();
   }
   next();
-});
+};
 
 // Restrict access to some operation
 exports.restrictTo = (...roles) => {
--- public/js/login.js
+++ public/js/login.js
@@ -23,3 +23,18 @@ export const login = async (email, password) => {
     showAlert('error', error.response.data.message);
   }
 };
+
+export const logout = async () => {
+  console.log('⛔⛔⛔');
+  try {
+    const res = await axios({
+      method: 'GET',
+      url: 'http://127.0.0.1:3000/api/v1/users/logout',
+    });
+    // We need to do location.reload(true), because we want to reaload page without cash
+    if (res.data.status === 'success') location.reload(true);
+  } catch (error) {
+    console.log(error.response);
+    showAlert('error', 'Error logging out! Try again.');
+  }
+};
